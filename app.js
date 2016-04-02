4f10b19f7ef2a99c586874946d74ef17e072fcb1
Alphonsa <Alphonsa>
2016-04-02 11:55:21 +0530

    env.port

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var port=process.env.PORT ||3000;
 var app = express();
 
 app.get('/index', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
